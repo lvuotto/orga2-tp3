0e3b90a0eb742e0a4f71192cf886d68a83c06848
lvuotto <lvuotto@dc.uba.ar>
2014-06-17 15:51:12 -0300

    Correciones de estilo.
    Cosas a tener en cuenta:
    - Hay múltiples comentarios desparramados por ahí que pueden ser útiles. Uno de los más importantes es el de `isr.asm`. Tiene un pequeño todo.
    - Leer toda la sección 3 del enunciado. Es __MUY__ importante.
    - Hay que hacer cosas relativas al juego, i.e., completar `game.c` (bah - ¿hay que hacerlo? Consultar). Eso NO se encuentra dentro del enunciado.
    
    Ponga huevo muchachos. Hoy esto se termina. No olvidemos que falta el informe. Ahora veo si lo relleno con algo.

--- src/sched.c
+++ src/sched.c
@@ -15,14 +15,6 @@ void scheduler_inicializar () {
 
 unsigned short sched_proximo_indice () {
   
-  unsigned short r;
-  
-  if (gdt[GDT_TSS_1].type & 0x2) {  /* gdt[GDT_TSS_1] busy? */
-    r = GDT_TSS_2;
-  } else {
-    r = GDT_TSS_1;
-  }
-  
   return r;
   
 }
